70c487d10d25cd50abd3c234256103c5a8e81ff9
Lasse Benninga <lassebenninga@godatadriven.com>
2022-01-30 19:37:05 +0100

    Update model

--- {{cookiecutter.project_name}}/models/model.py
+++ {{cookiecutter.project_name}}/models/model.py
@@ -1,13 +1,32 @@
-from dataclasses import dataclass, asdict
-import json
+from pydantic import BaseModel, Extra, validator
+from datetime import datetime
+import hashlib
+from typing import List, Optional
 
 
-@dataclass
-class Model:
-    name: str
+class Model(BaseModel, extra=Extra.allow):
+    title: str
+    datetime: str = datetime.now().isoformat()
+    id: Optional[str] = ""
 
-    def __post_init__(self):
-        pass
+    @validator("id", pre=True)
+    def set_id(cls, v, values):
+        return create_unique_id(values["title"])
 
-    def to_json(self):
-        return json.dumps(asdict(self))
+
+def create_unique_id(title: str):
+    return hashlib.md5(title.encode("utf-8")).hexdigest()
+
+
+def drop_duplicates(models: List[Model]):
+    unique_models: List[Model] = []
+    unique_ids = []
+
+    for model in models:
+        if model.id not in unique_ids:
+            unique_ids.append(model.id)
+            unique_models.append(model)
+        else:
+            continue
+
+    return unique_models
--- {{cookiecutter.project_name}}/scrape/scraper.py
+++ {{cookiecutter.project_name}}/scrape/scraper.py
@@ -1,28 +1,68 @@
-from typing import Dict
+from typing import Dict, List
 import requests
 import json
 
-url = ""
+from models.model import Model, drop_duplicates
 
-payload: Dict[str, str] = {}
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0",
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate, br",
-    "X-Requested-With": "XMLHttpRequest",
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Content-Length": "0",
-    "Connection": "keep-alive",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "TE": "trailers",
-}
 
-response = requests.request("POST", url, headers=headers, data=payload)
+def crawl():
+    url = ""
 
-_res = json.loads(response.text)
+    payload: Dict[str, str] = {}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "X-Requested-With": "XMLHttpRequest",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Content-Length": "0",
+        "Connection": "keep-alive",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "TE": "trailers",
+    }
 
-with open("response.json", "w") as f:
-    f.write(json.dumps(_res, indent=4))
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return json.loads(response.text)
+
+
+def parse_models(res_json: List[str]) -> List[Model]:
+    models: List[Model] = []
+
+    if res_json:
+        for entry in res_json:
+            model = Model(entry.title, entry.summary, entry.link)
+            models.append(model)
+
+    return models
+
+
+def drop_duplicates(path: str) -> List[Model]:
+    models: List[Model] = []
+
+    with open(path, "r") as f:
+        text = f.read()
+
+        if text:
+            json_models = json.loads(text)
+            for model_json in json_models:
+                models.append(
+                    Model(
+                        title=model_json["title"],
+                        summary=model_json["summary"],
+                        url=model_json["url"],
+                    )
+                )
+
+    return models
+
+
+def store_models(path: str, models: List[Model], as_json: bool = True):
+    with open(path, "w") as f:
+        if as_json:
+            models = [model.as_dict() for model in models]
+
+        f.write(json.dumps(models, default=str))
